b9d94a8d75ba59b04b12207c6b196640b42c86db
LostHurricane <77938349+LostHurricane@users.noreply.github.com>
2022-11-29 23:15:48 +0300

    Outliner presenter

---
+++ Assets/Scripts/UserControlSystem/UI/Presenter/SelectableOutlinerPresenter.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Abstractions;
+using UnityEngine;
+using UserControlSystem;
+
+public sealed class SelectableOutlinerPresenter : MonoBehaviour
+{
+    [SerializeField] private SelectableValue _selectedObject;
+    private Outline _currentOutline;
+
+    private void Start()
+    {
+        _selectedObject.OnSelected += ChangeSelection;
+    }
+
+    private void ChangeSelection(ISelectable selected)
+    {
+        if (selected is not MonoBehaviour selectable)
+        {
+            CleanCurrentOutline();
+            return;
+        }
+        if (selectable.TryGetComponent<Outline>(out _))
+        {
+            return;
+        }
+        CleanCurrentOutline();
+        var outline = selectable.gameObject.AddComponent<Outline>();
+        SetOutlineParameters(outline);
+
+        _currentOutline = outline;
+    }
+
+    private void CleanCurrentOutline()
+    {
+        if (_currentOutline != null)
+        {
+            Destroy(_currentOutline);
+        }
+    }
+
+    private void SetOutlineParameters(Outline outline)
+    {
+        outline.OutlineColor = Color.red;
+        outline.OutlineMode = Outline.Mode.OutlineVisible;
+        outline.OutlineWidth = 4f;
+    }
+
+    private void OnDestroy()
+    {
+        _selectedObject.OnSelected -= ChangeSelection;
+    }
+}
