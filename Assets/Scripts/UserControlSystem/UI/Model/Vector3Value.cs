85268da752b9c0091b611170a80a7baae34397e7
LostHurricane <77938349+LostHurricane@users.noreply.github.com>
2022-12-13 00:21:25 +0300

    task 3

--- Assets/Scripts/UserControlSystem/UI/Model/Vector3Value.cs
+++ Assets/Scripts/UserControlSystem/UI/Model/Vector3Value.cs
@@ -5,15 +5,7 @@ using UnityEngine;
 namespace UserControlSystem
 {
     [CreateAssetMenu(fileName = nameof(Vector3Value), menuName = "Strategy Game/" + nameof(Vector3Value), order = 0)]
-    public class Vector3Value : ScriptableObject
-    {
-        public Vector3 CurrentValue { get; private set; }
-        public Action<Vector3> OnNewValue;
-
-        public void SetValue(Vector3 value)
-        {
-            CurrentValue = value;
-            OnNewValue?.Invoke(value);
-        }
+    public class Vector3Value : SubscribableValueBaseClass<Vector3>
+    { 
     }
 }
