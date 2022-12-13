cee46be5a7c7b9d5e81219fb8b61ab9df1f05536
LostHurricane <77938349+LostHurricane@users.noreply.github.com>
2022-12-14 02:54:20 +0300

    hw

--- Assets/Scripts/Core/CommandExecutors/ProduceUnitComandExecutor.cs
+++ Assets/Scripts/Core/CommandExecutors/ProduceUnitComandExecutor.cs
@@ -1,14 +1,17 @@
 using Commands;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class ProduceUnitComandExecutor : CommandExecutorBase<IProduceUnitCommand>
 {
     [SerializeField] private Transform _unitsParent;
+    [SerializeField] private float _productionTime;
 
-    public override void ExecuteSpecificCommand(IProduceUnitCommand command)
+    public async override void ExecuteSpecificCommand(IProduceUnitCommand command)
     {
+        await Task.Delay((int)(_productionTime * 1000));
         Instantiate(command.UnitPrefab,
             new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)),
             Quaternion.identity,
