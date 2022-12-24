64d53b2c4e3e527d26ca4f0d91218e0fc03b423f
LostHurricane <77938349+LostHurricane@users.noreply.github.com>
2022-12-24 16:03:25 +0300

    task 1*

--- Assets/Scripts/UserControlSystem/UI/Presenter/MenuPresenter.cs
+++ Assets/Scripts/UserControlSystem/UI/Presenter/MenuPresenter.cs
@@ -8,11 +8,23 @@ public class MenuPresenter : MonoBehaviour
 {
     [SerializeField] private Button _backButton;
     [SerializeField] private Button _exitButton;
+
     private void Start()
     {
+        
         _backButton.OnClickAsObservable().Subscribe(_ =>
         gameObject.SetActive(false));
         _exitButton.OnClickAsObservable().Subscribe(_ =>
         Application.Quit());
+    }
+
+    private void OnEnable()
+    {
+        Time.timeScale = 0;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 1;
     }
 }
